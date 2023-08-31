c46139441c86007222c0aee749fedd3f5d2f9032
bhedafr <143655065+bhedafr@users.noreply.github.com>
2023-08-31 15:31:26 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Categories and Names</title>
+</head>
+<body>
+    <h1>Hello World PHP </h1>
+
+    <?php
+    echo 'Hello, World!';
+    ?>
+</body>
+</html>
