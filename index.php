48576a5bfe25ee801503038171b43e59c46581e7
bhedafr <143655065+bhedafr@users.noreply.github.com>
2023-09-01 12:27:56 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,21 @@
 
     <?php
         echo 'Hello, World!';
+
+        // PHP Data Objects(PDO) Sample Code:
+        try {
+            $conn = new PDO("sqlsrv:server = tcp:it-ccdv-sql-freyatest.database.windows.net,1433; Database = IT-CCDV-DB-FreyaTest", "rootAdmin", "{Sheridan!321}");
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e) {
+            print("Error connecting to SQL Server.");
+            die(print_r($e));
+        }
+        
+        // SQL Server Extension Sample Code:
+        $connectionInfo = array("UID" => "rootAdmin", "pwd" => "{Sheridan!321}", "Database" => "IT-CCDV-DB-FreyaTest", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+        $serverName = "tcp:it-ccdv-sql-freyatest.database.windows.net,1433";
+        $conn = sqlsrv_connect($serverName, $connectionInfo);
     ?>
 </body>
 </html>
